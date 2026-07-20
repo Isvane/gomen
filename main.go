7fd4fcd6343b4bc1052400d21f92c219bffbcfdb
Isvane <277444536+Isvane@users.noreply.github.com>
2026-07-20 13:35:35 +0700

    feat: add GET and DELETE user endpoints

--- main.go
+++ main.go
@@ -31,6 +31,37 @@ func (d *Database) registerUserHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Successfully registered %s with age %d", name, age)
 }
 
+func (d *Database) getUserHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	value, ok := d.UserInfo[name]
+	if ok {
+		fmt.Fprintf(w, "Found %q! Age: %d", name, value)
+	} else {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+}
+
+func (d *Database) deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	_, ok := d.UserInfo[name]
+	if ok {
+		delete(d.UserInfo, name)
+		fmt.Fprintf(w, "Deleted %q", name)
+	} else {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+}
+
 func echoHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.PathValue("arg")))
 }
@@ -43,6 +74,8 @@ func main() {
 
 	mux.HandleFunc("GET /echo/{arg}", echoHandler)
 	mux.HandleFunc("POST /register/{name}/{age}", db.registerUserHandler)
+	mux.HandleFunc("GET /user/{name}", db.getUserHandler)
+	mux.HandleFunc("DELETE /user/{name}", db.deleteUserHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
